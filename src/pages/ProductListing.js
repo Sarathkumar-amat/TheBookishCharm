cc9c9df83cd2be486af0d747a1acb99c1023486a
Sarathkumar-amat <poongodinall@gmail.com>
2023-05-16 00:10:03 +0530

    applied Category Filter

--- src/pages/ProductListing.js
+++ src/pages/ProductListing.js
@@ -3,18 +3,26 @@ import "../App.css";
 
 import { ProductContext } from "../contexts/ProductProvider";
 import { CategoryContext } from "../contexts/CategoryProvider";
+import { CheckAndApplyFilter } from "../Filters/CheckandApplyFilter";
 
 export function ProductListing()
 {
-    const {bookState} = useContext(ProductContext);
+    const {bookState,dispatch} = useContext(ProductContext);
     const bookStyle = {
         border:"1px solid black",
         margin:"5px"
     }
     console.log(bookState.displayBooks);
+    let displayValue = bookState.displayBooks;
+    displayValue = CheckAndApplyFilter(bookState.categoryFilters);
+    
     return (<div>
         <h1>List of all products</h1>
-        <ul id="bookList">{bookState?.displayBooks?.map(({title,image,price,categoryName})=>
+       <ul> {bookState.categories?.map(category=>
+            <li>
+                <input type="checkbox" onClick={()=>dispatch({type:"catFilter",payload:category})} />{category}
+            </li>)}</ul>
+        <ul id="bookList">{displayValue?.map(({title,image,price,categoryName})=>
         <li style={bookStyle}>
             <img height="200px" width="200px" src={image} alt={title}/>
             <p>Title: {title}</p>
