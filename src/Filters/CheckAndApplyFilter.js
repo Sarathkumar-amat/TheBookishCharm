7e886a37600f5eb1393bdbf34ee4aaad5117aadd
Sarathkumar-amat <poongodinall@gmail.com>
2023-05-16 08:04:12 +0530

    filter

---
+++ src/Filters/CheckAndApplyFilter.js
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import { ProductContext } from "../contexts/ProductProvider";
+
+export function ApplyCategoryFilter(categoryFilterArr)
+{
+    const {bookState} = useContext(ProductContext);
+    let displayValue=bookState.displayBooks;
+    const newData = categoryFilterArr?.reduce((initVal,current)=>{
+        const booksData = bookState.allBooks?.filter(({categoryName})=>categoryName===current)
+        return [...initVal,...booksData]
+    },[])
+    if(newData?.length>0)
+    {
+        displayValue=newData;
+    }
+    return displayValue;
+    
+}
